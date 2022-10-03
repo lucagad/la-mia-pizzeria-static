97c86a9069beeec3f1d6656ff433a9832997fa59
lucagaddini <lucagaddini@icloud.com>
2022-10-03 13:52:56 +0200

    Inizializzazione Repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace la_mia_pizzeria_static.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
